b35d11c4af872b61067357b2651b2fd33a03c865
Ali Torabi <alithema@gmail.com>
2023-08-07 13:45:41 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,11 @@
 function insertionSortReverse(arr) {
-    for(var i = arr.length - 1; i > 0; i--) {
+  for(var i = 1; i < arr.length; i++) {
     var val = arr[i];
     var j;
-    for(j = i; j < arr.length && arr[j+1] < val; j++) {
-      arr[j] = arr[j+1];
+    for(j = i; j > 0 && arr[j-1] > val; j--) {
+      arr[j] = arr[j-1];
     }
     arr[j] = val;
   }
-    return arr;
+  return arr;
 }
